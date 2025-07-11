70154923fab5a6886570bfd52e775444351d5ea0
Aditya10112002 <80091696+Aditya10112002@users.noreply.github.com>
2025-07-11 14:53:26 +0530

    delete login

--- dhansetu-backend/src/main/java/com/sk/dhansetu/auth/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sk.dhansetu.auth.repository;
-
-import com.sk.dhansetu.auth.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsernameAndPassword(String username, String password);
-}
