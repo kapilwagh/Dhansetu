2ccb6318520debe814929b8bd71452fc92777fc7
kapilwagh <kapil.wagh@somaiya.edu>
2025-07-11 12:21:50 +0530

    backend-structure

---
+++ dhansetu-backend/src/main/java/com/sk/dhansetu/DhansetuApplication.java
@@ -0,0 +1,13 @@
+package com.sk.dhansetu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhansetuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhansetuApplication.class, args);
+	}
+
+}
---
+++ dhansetu-backend/src/test/java/com/sk/dhansetu/DhansetuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sk.dhansetu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhansetuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
