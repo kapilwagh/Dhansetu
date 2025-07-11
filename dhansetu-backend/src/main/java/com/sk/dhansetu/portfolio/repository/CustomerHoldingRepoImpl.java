8fe1c37d1d77b961ddbb3664b740047396e03de6
kapilwagh <kapil.wagh@somaiya.edu>
2025-07-12 03:08:04 +0530

    analysis & rm dashboard api

--- dhansetu-backend/src/main/java/com/sk/dhansetu/portfolio/repository/CustomerHoldingRepoImpl.java
+++ dhansetu-backend/src/main/java/com/sk/dhansetu/portfolio/repository/CustomerHoldingRepoImpl.java
@@ -1,25 +1,7 @@
 package com.sk.dhansetu.portfolio.repository;
 
-import com.sk.dhansetu.portfolio.model.CustomerHolding;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public class CustomerHoldingRepoImpl {
-
-    @Autowired
-    private CustomerHoldingRepository customerHoldingRepository;
-
-
-    public List<CustomerHolding> findByCustomerIdRepo(Long customerId) {
-        return customerHoldingRepository.findByCustomerId(customerId);
-    }
-
-
-
-
-
-
 }
